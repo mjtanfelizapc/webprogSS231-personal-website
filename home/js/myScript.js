2e050738a1211c82dab79dfa7f3f2469f0d807e7
mjtanfelizapc <micharltanfeliz6@gmail.com>
2024-12-17 09:03:30 +0800

    update

--- home/js/myScript.js
+++ home/js/myScript.js
@@ -4,13 +4,11 @@ function myFunction() {
 
 
 
-function myFunction2() {
-    const element = document.getElementsByTagName("h1");
 
-    function myFunction2() {
-        const element = document.getElementsByTagName("h1");
+function myFunction2() {
+    const element = document.getElementsByTagName("section");
      
-        document.getElementById("demo").innerHTML = 'The text in first paragraph (index 0) is: ' + element[0].innerHTML;
+     document.getElementById("demo").innerHTML = 'The text in first paragraph (index 0) is: ' + element[0].innerHTML;
      
      
 }
