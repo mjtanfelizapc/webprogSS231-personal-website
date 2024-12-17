ff029bcaacd68bfae72a44618889d89b2af86099
mjtanfelizapc <micharltanfeliz6@gmail.com>
2024-12-17 08:29:41 +0800

    update

--- home/js/myScript.js
+++ home/js/myScript.js
@@ -1,3 +1,11 @@
-function myfunction() {
+function myFunction() {
     document.getElementById("demo").innerHTML="Paragraph changed.";
+}
+
+
+
+function myFunction2() {
+    const element = document.getElementsByTagName("h1");
+
+    document.getElementById("demo").innerHTML = 'The text in first paragraph <h1> tag (index 0) is: ' + element[0].innerHTML;
 }
