e74e3b3c5bd589909bdf45ce745fa68b2c08ef2f
mjtanfelizapc <micharltanfeliz6@gmail.com>
2024-12-17 08:39:10 +0800

    updated

--- home/js/myScript.js
+++ home/js/myScript.js
@@ -7,5 +7,10 @@ function myFunction() {
 function myFunction2() {
     const element = document.getElementsByTagName("h1");
 
-    document.getElementById("demo").innerHTML = 'The text in first paragraph <h1> tag (index 0) is: ' + element[0].innerHTML;
+    function myFunction2() {
+        const element = document.getElementsByTagName("h1");
+     
+        document.getElementById("demo").innerHTML = 'The text in first paragraph (index 0) is: ' + element[0].innerHTML;
+     
+     
 }
