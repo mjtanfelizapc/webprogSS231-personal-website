0da879967d397a53e72e39a7cd9cc6b915aa6e4d
mjtanfelizapc <micharltanfeliz6@gmail.com>
2025-01-13 09:03:45 +0800

    update 14

--- home/js/vue.js
+++ home/js/vue.js
@@ -2,8 +2,12 @@ document.addEventListener("DOMContentLoaded", () => {
     const app = Vue.createApp({
         data() {
             return {
+                backgroundColor: '#f0f8ff', // Default background color
                 comments: [],
-                newComment: { name: '', comment: '' }
+                newComment: {
+                    name: '',
+                    comment: ''
+                }
             };
         },
         methods: {
@@ -13,23 +17,34 @@ document.addEventListener("DOMContentLoaded", () => {
                     this.newComment.name = '';
                     this.newComment.comment = '';
                 }
+            },
+            changeBackgroundColor() {
+                // Array of background options (colors or image URLs)
+                const colorsOrImages = [
+                    '#f0f8ff', 
+                    '#ffebcd', 
+                    '#add8e6', 
+                    "url('https://img.freepik.com/free-photo/japan-background-digital-art_23-2151546124.jpg')", // << HIGHLIGHTED LINE: Add your URL here
+                    "url('https://img.freepik.com/free-photo/illustration-anime-city_23-2151779669.jpg')"
+                ];
+                // Randomly pick a color or image URL
+                this.backgroundColor = colorsOrImages[Math.floor(Math.random() * colorsOrImages.length)];
             }
         }
     });
 
     app.component('image-gallery', {
-        template: `
-            <div class="image-gallery">
-                <img src="image1.jpg" alt="Gallery Image 1">
-                <img src="image2.jpg" alt="Gallery Image 2">
-                <img src="image3.jpg" alt="Gallery Image 3">
-            </div>
-        `
+        template: 
+            `<div class="image-gallery">
+                <img src="https://via.placeholder.com/300" alt="Image 1">
+                <img src="https://via.placeholder.com/300" alt="Image 2">
+                <img src="https://via.placeholder.com/300" alt="Image 3">
+            </div>`
     });
 
     app.component('guestbook-form', {
-        template: `
-            <div>
+        template: 
+            `<div>
                 <form @submit.prevent="submitComment">
                     <input v-model="name" type="text" placeholder="Your Name" required>
                     <textarea v-model="message" placeholder="Your Comment" required></textarea>
@@ -40,10 +55,13 @@ document.addEventListener("DOMContentLoaded", () => {
                         <strong>{{ comment.name }}</strong>: {{ comment.message }}
                     </li>
                 </ul>
-            </div>
-        `,
+            </div>`,
         data() {
-            return { name: '', message: '', comments: [] };
+            return {
+                name: '',
+                message: '',
+                comments: []
+            };
         },
         methods: {
             submitComment() {
